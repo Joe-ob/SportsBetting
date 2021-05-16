61083b9bc444d31ceeafa1db16321ee789cf0c94
JoeObrien <jpo52@georgetown.edu>
2021-05-16 16:16:46 -0400

    Odds_check

--- odds_check.py
+++ odds_check.py
@@ -1,3 +1,9 @@
 from odds_seeker import Match_Team
 
-Mock_list_1 = ["New", "York", "Yankees"]
+Mock_list_1 = ["a", "b", "c"]
+Mock_list_2 = ["f", "e", "d", "c", "b", "a"]
+Mock_list_3 = ["x", "y", "z"]
+
+def Test_if_Teams_Match():
+    assert Match_Team(Mock_list_1, Mock_list_2) == True
+    assert Match_Team(Mock_list_3, Mock_list_2) == False
